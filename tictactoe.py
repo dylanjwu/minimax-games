50753d510dc219d575cefcc4b8bbb7c1a6cf378b
[dylanjwu] <dylanjwu@gmail.com>
2022-01-14 11:10:08 -0500

    alpha beta seems to work\!

--- tictactoe.py
+++ tictactoe.py
@@ -1,151 +1,111 @@
 
 import sys
-board = [
-    ['o', '#','#'],
-    ['#', '#','#'],
-    ['x', '#','o']
-]
 
-def isFull(board):
-    for arr in board:
-        for el in arr:
-            if el == '#':
-                return False
-    return True
-
-def winsDiagonal(board, player):
-    left = board[0][0] == board[1][1] == board[2][2] == player
-    right = board[0][2] == board[1][1] == board[2][0] == player
-    return left or right
-
-def winsColumn(board, player):
-    transpose = [['' for _ in board] for _ in board]
-    for i in range(len(board)):
-        transpose[i] = []
-        for j in range(len(board[i])):
-            transpose[i].append(board[j][i])  
-    return winsRow(transpose, player)
-
-def winsRow(board, player):
-    for row in board:
-        if row == [player, player, player]:
-            return True
-    return False
-
-def isWin(board, player):
-    return winsRow(board, player) or winsColumn(board, player) or winsDiagonal(board, player)
-
-def allBoards(board):
-    win_boards = []
-    draw_boards = []
-
-    def allTTTBoards(board, isO=True):
-
-        player = 'o' if isO else 'x'
-
-        if isWin(board, 'o') or isWin(board, 'x'):
-            win_boards.append(board)
-            return 
-
-        elif isFull(board):
-            draw_boards.append(board) 
-            return 
-
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                boardCopy = [row[:] for row in board]
-                if boardCopy[i][j] == '#':
-                    boardCopy[i][j] = player
-                    # print(boardCopy)
-                    allTTTBoards(boardCopy, not isO)
-
-    allTTTBoards(board, True)
-    return {"wins": win_boards, "draws": draw_boards}
-
-BOARDS = []
 COMP = 'o'
 PLAYER = 'x'
 
-def evaluate(board):
-    if isWin(board, PLAYER):
-        BOARDS.append(board)
-        return (1, board)
+class MiniMax:
 
-    elif isWin(board, COMP):
-        return (-1, board)
+    def __init__(self, board):
+        self.board = board #original board; should not be modified
+        self.indices = [(i,j) for i in range(len(board)) for j in range(len(board))]
 
-    elif isFull(board):
-        return (0, board)
+    def isFull(self, board):
+        for arr in board:
+            for el in arr:
+                if el == '#':
+                    return False
+        return True
 
-    return ()
+    def winsDiagonal(self, board, player):
+        left = board[0][0] == board[1][1] == board[2][2] == player
+        right = board[0][2] == board[1][1] == board[2][0] == player
+        return left or right
 
+    def winsColumn(self, board, player):
+        transpose = [['' for _ in board] for _ in board]
+        for i in range(len(board)):
+            transpose[i] = []
+            for j in range(len(board[i])):
+                transpose[i].append(board[j][i])  
+        return self.winsRow(transpose, player)
 
+    def winsRow(self, board, player):
+        for row in board:
+            if row == [player, player, player]:
+                return True
+        return False
 
-def alpha_beta(board):
-    return max_value(board, (), -sys.maxsize, sys.maxsize)
-    
+    def isWin(self, board, player):
+        return (self.winsRow(board, player) or self.winsColumn(board, player) 
+            or self.winsDiagonal(board, player))
 
-def min_value(board, moves, alpha, beta):
+    def evaluate(self, board):
+        if self.isWin(board, PLAYER):
+            return (-1, board)
 
-    if (e := evaluate(board)) != ():
-        return (e[0], e[1], moves)
+        elif self.isWin(board, COMP):
+            return (1, board)
 
-    res = sys.maxsize
-    final_board = []
-    best_moves = ()
+        elif self.isFull(board):
+            return (0, board)
 
-    for i in range(len(board)): 
-        end_loop = False
-        for j in range(len(board)):
-            if board[i][j] == '#':
-                boardCopy = [a[:] for a in board]
+        return ()
+
+    def alpha_beta(self):
+        return self.ab_helper(self.board, [], -sys.maxsize, sys.maxsize, False) 
+
+    def ab_helper(self, board, moves, alpha, beta, is_min):
+        if (e := self.evaluate(board)) != ():
+            return (e[0], e[1], [])
+
+        res = sys.maxsize if is_min else -sys.maxsize
+        final_board, best_moves = [], []
+        val = res
+
+        for position in self.indices:
+            i, j = position
+
+            if board[i][j] != '#':
+                continue
+
+            boardCopy = [a[:] for a in board]
+
+            if is_min:
                 boardCopy[i][j] = PLAYER
-                val, b, moves = max_value(boardCopy, moves + ((i, j)), alpha, beta)                
+                val, b, moves = self.ab_helper(boardCopy, moves[::], alpha, beta, False)                
                 if val < res:
                     res = val
-                    best_moves = moves
+                    best_moves = [(i, j)] + moves
                     final_board = b
 
                 beta = min(res, beta)
 
-        #         if res <= alpha:
-        #             end_loop = True
-        #             break 
-        # if end_loop:
-        #     break
+                if res <= alpha:
+                    break 
 
-    return (res, final_board, best_moves)
-
-def max_value(board, moves, alpha, beta):
-
-    if (e := evaluate(board)) != ():
-        return (e[0], e[1], moves)
-
-    res = -sys.maxsize
-    final_board = []
-    best_moves = ()
-    for i in range(len(board)): 
-        end_loop = False
-        for j in range(len(board)):
-            if board[i][j] == '#':
-                boardCopy = [a[:] for a in board]
+            else:
                 boardCopy[i][j] = COMP
-                val, b, moves = min_value(boardCopy, moves + ((i, j)), alpha, beta)                
+                val, b, moves = self.ab_helper(boardCopy, moves[::], alpha, beta, True)                
                 if val > res:
                     res = val
+                    best_moves = [(i, j)] + moves
                     final_board = b
-                    best_moves = moves
-                        
+
                 alpha = max(res, alpha)
 
-                # if res >= beta:
-                #     end_loop = True
-                #     break 
+                if res >= beta:
+                    break 
 
-        # if end_loop:
-        #     break
+            # minimize the depth
+            if val == res:
+                if len(moves) < len(best_moves):
+                    best_moves = [(i, j)] + moves
+                    final_board = b
 
-    return (res, final_board, best_moves)    
+
+        return (res, final_board, best_moves)
+
 
 boards = [
     [
@@ -170,8 +130,40 @@ boards = [
     ],
 ]
 
-res = alpha_beta(boards[2])
-print(res)
-# print(BOARDS)
-# for b in BOARDS:
-#     print(str(b) + '\n')
+move_2_boards = []
+board_template = [['#']*3 for _ in range(3)]
+for i in range(3):
+    for j in range(3):
+        new_board = [arr[::] for arr in board_template]
+        new_board[i][j] = 'x'
+        curr = (i, j)
+        for k in range(3):
+            for l in range(3):
+                if (k, l) != curr:
+                    new_1_board = [arr[::] for arr in new_board]
+                    new_1_board[k][l] = 'x'
+                    if not new_board in move_2_boards:
+                        move_2_boards.append(new_1_board)
+
+def print_board(board):
+    for row in board:
+        print(str(row))
+
+def test_boards(boards):
+
+    for i in range(len(boards)):
+        print("\n\nORIGINAL BOARD: ")
+        print_board(boards[i])
+        MM = MiniMax(boards[i])
+        res = MM.alpha_beta()
+        print("Make the move: " + str(res[2][0]))
+        outcome_str = "Draw!"
+        if res[0] == -1:
+            outcome_str = "Player (" + PLAYER +  ") wins"
+        elif res[0] == 1:
+            outcome_str = "Computer (" + COMP +  ") wins"
+        print(f'OUTCOME: {outcome_str}')
+        print(f'RESULTING BOARD:')
+        print_board(res[1])
+
+test_boards(move_2_boards)
